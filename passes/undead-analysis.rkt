3b90bd729a5f3272fe321ef66952051870b2027c
korih <96402323+korih@users.noreply.github.com>
2025-02-16 16:59:22 -0800

    Undead done, x64-wrapper started
    undead analysis appears done

--- passes/undead-analysis.rkt
+++ passes/undead-analysis.rkt
@@ -2,19 +2,19 @@
 
 (require
   cpsc411/compiler-lib
-  cpsc411/langs/v2
-  cpsc411/langs/v2-reg-alloc
+  cpsc411/langs/v4
   rackunit)
 
 (provide undead-analysis)
 
-;; Exercise 1
 ;; asm-lang-v2/locals -> asm-lang-v2/undead
 ;; compiles p to asm-lang-v2/undead by performing undeadness analysis,
 ;; decorating the program with undead-set tree
 (define/contract (undead-analysis p)
-  (-> asm-lang-v2/locals? asm-lang-v2/undead?)
+  (-> asm-pred-lang-v4/locals? asm-pred-lang-v4/undead?)
 
+  ;; (asm-pred-lang-v4/locals tail) -> (ListOf undead-set-tree) (Listof undead-in-set)
+  ;; go through the tail and analyze the undead sets
   (define (analyze-tail t)
     (match t
       [`(begin ,effects ... ,tail)
@@ -30,10 +30,28 @@
              (analyze-effects effect undead-out))
            (values (cons ust rev-ust) undead-in)))
        (values rev-ust undead-in)]
+
+      [`(if ,pred ,t1 ,t2)
+       (define-values (t1-ust t1-undead-out) (analyze-tail t1))
+       (define-values (t2-ust t2-undead-out) (analyze-tail t2))
+
+       ;; union the set of undead-outs
+       (define tail-undead-in (set-union t1-undead-out t2-undead-out))
+
+       ;; cons new ust to ust
+       (define ust^ (list t1-ust t2-ust))
+
+       ;; analyze pred might need to take undead-in
+       (define-values (p-ust p-undead-out) (analyze-pred pred tail-undead-in))
+
+       (values (cons p-ust ust^) p-undead-out)]
+
       [`(halt ,triv) (define undead-in
                        (analyze-triv triv))
                      (values '() undead-in)]))
 
+  ;; (asm-pred-lang-v4/locals effects) -> (ListOf undead-set-tree) (Listof undead-in-set)
+  ;; look through effects and create a undead-set-tree and undead-in-set for current effect
   (define (analyze-effects e undead-out)
     (match e
       [`(begin ,effects ...)
@@ -56,13 +74,65 @@
        (define undead-in (set-union
                           (set-remove undead-out aloc)
                           (analyze-triv triv)))
-       (values undead-out undead-in)]))
+       (values undead-out undead-in)]
+      [`(if ,pred ,e1 ,e2)
+       (define-values (e1-ust e1-undead-out) (analyze-effects e1 undead-out))
+       (define-values (e2-ust e2-undead-out) (analyze-effects e2 undead-out))
 
+       (define effect-undead-in (set-union e1-undead-out e2-undead-out))
+       (define ust (list e1-ust e2-ust))
+
+       (define-values (p-ust p-undead-out) (analyze-pred pred effect-undead-in))
+
+       (values (cons ust p-ust) p-undead-out)]))
+
+  ;; (asm-pred-lang-v4/locals pred) -> (ListOf undead-set-tree) (Listof undead-in-set)
+  ;; go through the tail and analyze the undead sets
+  (define (analyze-pred p undead-in)
+    (match p
+      [`(,relop ,aloc ,triv)
+       ;; reference so we know aloc and/or triv are undead here
+       (define undead-out (set-union (set-add undead-in aloc) (analyze-triv triv)))
+       (values undead-in undead-out)]
+      [`(not ,pred)
+       (define-values (ust undead-out) (analyze-pred pred))
+       (values ust undead-out)]
+      [`(begin ,effects ... ,pred)
+       (define-values (p-out p-undead-out)
+         (analyze-pred pred))
+
+       (define-values (e-ust e-undead-out)
+         (for/foldr ([ust^ (list p-out)]
+                     [undead-in^ p-undead-out])
+           ([effect effects])
+           (define-values (ust undead-in)
+             (analyze-effects effect undead-in^))
+           (values (cons ust ust^) undead-in)))
+       (values e-ust e-undead-out)]
+
+      [`(if ,p1 ,p2 ,p3)
+       (define-values (p3-ust p3-undead-out) (analyze-pred p3 undead-in))
+       (define-values (p2-ust p2-undead-out) (analyze-pred p2 undead-in))
+
+       (define pred-undead-in (set-union p3-undead-out p2-undead-out))
+       (define ust (list p3-ust p2-ust))
+
+       (define-values (p1-ust p1-undead-out) (analyze-pred p1 pred-undead-in))
+       (values (cons p1-ust ust) p1-undead-out)] ; just call pred for each one then combine?
+
+      ;; catch case where pred is true or false
+      [_ '()]))
+
+  ;; (asm-pred-lang-v4/locals triv) -> (Listof undead-in-set)
+  ;; analyze the triv value, if its an aloc return it as a list
+  ;; for the undead-in set, if not then return empty list
   (define (analyze-triv triv)
     (match triv
       [x #:when (aloc? x) (list x)]
       [_ '()]))
 
+  ;; info (asm-pred-lang-v4/locals tail) -> (Listof undead-in-set)
+  ;; analyze the tail and add the undead set tree to the info
   (define (compile-info i tail)
     (match i
       [`,info
@@ -211,4 +281,41 @@
                     (set! p.1 -1)
                     (set! t.6 (* t.6 p.1))
                     (set! z.5 (+ z.5 t.6))
-                    (halt z.5)))))
+                    (halt z.5))))
+ (check-equal? (undead-analysis '(module
+                                     ((locals (x.1 y.2 b.3 c.4)))
+                                   (begin
+                                     (set! x.1 5)
+                                     (set! y.2 x.1)
+                                     (begin
+                                       (set! b.3 x.1)
+                                       (set! b.3 (+ b.3 y.2))
+                                       (set! c.4 b.3)
+                                       (if (= c.4 b.3)
+                                           (halt c.4)
+                                           (begin
+                                             (set! x.1 c.4)
+                                             (halt c.4)))))))
+               '(module
+                    ((locals (x.1 y.2 b.3 c.4))
+                     (undead-out ((x.1)
+                                  (x.1 y.2)
+                                  ((y.2 b.3)
+                                   (b.3)
+                                   (b.3 c.4)
+                                   ((c.4)
+                                    ()
+                                    ((c.4)
+                                     ()))))))
+                  (begin
+                    (set! x.1 5)
+                    (set! y.2 x.1)
+                    (begin
+                      (set! b.3 x.1)
+                      (set! b.3 (+ b.3 y.2))
+                      (set! c.4 b.3)
+                      (if (= c.4 b.3)
+                          (halt c.4)
+                          (begin
+                            (set! x.1 c.4)
+                            (halt c.4))))))))
--- x64-wrapper.rkt
+++ x64-wrapper.rkt
@@ -1,13 +1,15 @@
 #lang racket
 
 (require
-  cpsc411/compiler-lib)
+  cpsc411/compiler-lib
+  cpsc411/langs/v4)
 
 (provide generate-x64)
 
 ;; paren-x64-v1 -> x64-instruction-sequence
 ;; interp. a compiler that generates x64 instructions for the paren-x64-v2 language
-(define (generate-x64 p)
+(define/contract (generate-x64 p)
+  (-> paren-x64-v4? string?)
   ;; paren-x64-v2 -> x64-instruction-sequence
   ;; interp. a compiler that generates x64 instructions for the paren-x64-v2 language
   (define (program->x64 p)
@@ -37,11 +39,16 @@
   ;; interp. a generated x64-instruction-sequence for a paren-x64-v2 statement
   (define (statement->x64 s)
     (match s
+      ;; TODO: case where (addr reg) is now to (addr trg) for setting label
       [`(set! (,fbp - ,dispoffset) ,val) (format "mov QWORD [~a - ~a], ~a" fbp dispoffset val)]
       [`(set! ,reg ,val) #:when (int64? val) (format "mov ~a, ~a" reg val)]
       [`(set! ,reg1 ,loc) #:when (loc? loc) (format "mov ~a, ~a" reg1 (loc->x64 loc))]
       [`(set! ,reg_1 (,op ,reg_1 ,val)) #:when (int32? val) (format "~a ~a, ~a" (binop->ins op) reg_1 val)]
-      [`(set! ,reg_1 (,op ,reg_1 ,loc)) (format "~a ~a, ~a" (binop->ins op) reg_1 (loc->x64 loc))]))
+      [`(set! ,reg_1 (,op ,reg_1 ,loc)) (format "~a ~a, ~a" (binop->ins op) reg_1 (loc->x64 loc))]
+      [`(with-label ,label ,s) '...]
+      [`(jump ,trg) '...]
+      [`(compare ,reg ,op) '...]
+      [`(jump-if ,relop ,label) '...]))
 
 
   ;; paren-x64-v2 binop -> x64-instruction-sequence instruction binop
@@ -50,5 +57,11 @@
     (match b
       [`+ "add"]
       [`* "imul"]))
+
+  ;; paren-x64 trg -> ...
+  ;; TODO:
+  (define (resolve-trg trg)
+    (match trg
+      [`,reg '...]))
 
   (program->x64 p))
