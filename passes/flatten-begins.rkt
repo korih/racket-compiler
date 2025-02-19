d1755f55c82a6b2ce5224150385d2e6e0d63401e
itsarune <42703774+itsarune@users.noreply.github.com>
2025-02-19 13:26:06 -0800

    finish making tests

--- passes/flatten-begins.rkt
+++ passes/flatten-begins.rkt
@@ -34,17 +34,36 @@
 
 (test-case
  "flatten-begins"
- (check-equal? (flatten-begins '(module (halt 1))) '(begin (halt 1)))
- (let ([x (symbol->string (gensym))])
+ (let ([x (fresh-label)])
+     (check-equal? (flatten-begins `(module (define ,x (halt 1)))) '(begin (halt 1))))
+ (let ([x (fresh-label)])
    (check-equal? (flatten-begins `(module (define ,x (begin (set! rbx 1) (halt rbx))))
                                 '(begin (with-label ,x (set! rbx 1)) (halt rbx)))))
- (let ([x (symbol->string (gensym))])
+ (let ([x (fresh-label)])
    (check-equal? (flatten-begins `(module (define ,x (begin (set! rax 0) (begin (set! rbx 1) (halt rbx))))))
                  `(begin (with-label ,x (set! rax 0)) (set! rbx 1) (halt rbx))))
- (let ([x (symbol->string (gensym))])
+ (let ([x (fresh-label)])
    (check-equal? (flatten-begins `(module (define ,x (begin (begin (set! rax 0)) (halt rax))))
                                  `(begin (with-label ,x (set! rax 0)) (halt rax)))))
- (let ([x (symbol->string (gensym))]
-       [y (symbol->string (gensym))])
+ (let ([x (fresh-label)]
+       [y (fresh-label)])
    (check-equal? (flatten-begins `(module (define ,x (jump ,y)) (define ,y (halt 0))))
-                 `(begin (with-label ,x (jump ,y)) (with-label ,y (halt 0))))))
+                 `(begin (with-label ,x (jump ,y)) (with-label ,y (halt 0)))))
+ (let ([x (fresh-label)]
+       [y (fresh-label)]
+       [z (fresh-label)])
+   (check-equal? (flatten-begins `(module (define ,x (if (< r1 0)) (jump ,y) (jump ,z))
+                                    (define ,y (halt r0))
+                                    (define ,z (set! r1 (+ r1 -1)) (jump ,x))))
+                 `(begin (with-label ,x (compare r1 0) (jump-if < ,y))
+                    (with-label ,y (halt r0))
+                    (with-label ,z (set! r1 (+ r1 -1)) (jump ,x)))))
+ (let ([x (fresh-label)]
+       [y (fresh-label)]
+       [z (fresh-label)])
+   (check-equal? (flatten-begins `(module (define ,x (set! r1 0) (jump ,y))
+                                    (define ,y (set! r2 0) (jump ,z))
+                                    (define ,z (halt r0)))
+                 `(begin (with-label ,x (set! r1 0) (jump ,y))
+                         (with-label ,y (set! r2 0) (jump ,z))
+                         (with-label ,z (halt r0)))))))
