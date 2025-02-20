74f2864c784233f29811b60b4a427e1dac2b56f5
arun <arun.sfc@outlook.com>
2025-02-19 18:45:48 -0800

    Oops misunderstood M4

--- passes/flatten-begins.rkt
+++ passes/flatten-begins.rkt
@@ -2,33 +2,17 @@
 
 (require
   cpsc411/compiler-lib
-  cpsc411/langs/v4
+  cpsc411/langs/v2
   rackunit)
 
 (provide flatten-begins)
 
-;; block-asm-lang-v4 -> para-asm-lang-v4
+;; nested-asm-lang-v2 -> para-asm-lang-v2
 ;; interp. flatten begin statements in the program
 (define/contract (flatten-begins p)
-  (-> block-asm-lang-v4? para-asm-lang-v4?)
+  (-> nested-asm-lang-v2? para-asm-lang-v2?)
 
-  ;; block-asm-lang-v4-b -> (list para-asm-lang-v4-s)
-  ;; convert b expressions to flattened s expressions
-  (define (flatten-begins/b b)
-    (match b
-      [`(define ,label ,tail)
-       (define compiled-s (flatten-begins/tail tail))
-       (list `(with-label ,label (first ,compiled-s)) (rest compiled-s))]))
-
-  ;; block-asm-lang-v4-tail -> (list para-asm-lang-v4-s)
-  ;; interp. flattens the tail expression into a list of s statements
-  (define (flatten-begins/tail tail)
-    (match tail
-      [`(halt ,opand) (void)]
-      [`(jump ,trg) (void)]
-      [`(begin ,fx ... ,tail) (void)]
-      [`(if (,relop ,loc ,opand) (jump ,trg1) (jump ,trg2)) (void)]))
-
+  ;; nested-asm-lang-v2-effect -> (list param-asm-lang-v2-effect)
   ;; interp. flatten begin statements in the program into a list of effect
   ;; statements
   (define (flatten-begins/effect e)
@@ -42,44 +26,17 @@
        (append compiled-fx (flatten-begins/effect e))]))
 
   (match p
-    [`(module ,bs ...)
-     (define compiled-s (for/fold ([s empty])
-                          ([b bs])
-                                  (append (flatten-begins/b b) s)))
-     `(module ,@compiled-s)]))
+    [`(halt ,triv) `(begin (halt ,triv))]
+    [`(begin ,fx ... ,tail)
+     (define compiled-fx (for/foldr ([fx-acc empty])
+                           ([e fx])
+                           (append (flatten-begins/effect e) fx-acc)))
+     (make-begin compiled-fx (flatten-begins tail))]))
 
 (test-case
  "flatten-begins"
- (let ([x (fresh-label)])
-     (check-equal? (flatten-begins `(module (define ,x (halt 1)))) '(begin (halt 1))))
- (let ([x (fresh-label)])
-   (check-equal? (flatten-begins `(module (define ,x (begin (set! rbx 1) (halt rbx))))
-                                '(begin (with-label ,x (set! rbx 1)) (halt rbx)))))
- (let ([x (fresh-label)])
-   (check-equal? (flatten-begins `(module (define ,x (begin (set! rax 0) (begin (set! rbx 1) (halt rbx))))))
-                 `(begin (with-label ,x (set! rax 0)) (set! rbx 1) (halt rbx))))
- (let ([x (fresh-label)])
-   (check-equal? (flatten-begins `(module (define ,x (begin (begin (set! rax 0)) (halt rax))))
-                                 `(begin (with-label ,x (set! rax 0)) (halt rax)))))
- (let ([x (fresh-label)]
-       [y (fresh-label)])
-   (check-equal? (flatten-begins `(module (define ,x (jump ,y)) (define ,y (halt 0))))
-                 `(begin (with-label ,x (jump ,y)) (with-label ,y (halt 0)))))
- (let ([x (fresh-label)]
-       [y (fresh-label)]
-       [z (fresh-label)])
-   (check-equal? (flatten-begins `(module (define ,x (if (< r1 0)) (jump ,y) (jump ,z))
-                                    (define ,y (halt r0))
-                                    (define ,z (set! r1 (+ r1 -1)) (jump ,x))))
-                 `(begin (with-label ,x (compare r1 0) (jump-if < ,y))
-                    (with-label ,y (halt r0))
-                    (with-label ,z (set! r1 (+ r1 -1)) (jump ,x)))))
- (let ([x (fresh-label)]
-       [y (fresh-label)]
-       [z (fresh-label)])
-   (check-equal? (flatten-begins `(module (define ,x (set! r1 0) (jump ,y))
-                                    (define ,y (set! r2 0) (jump ,z))
-                                    (define ,z (halt r0)))
-                 `(begin (with-label ,x (set! r1 0) (jump ,y))
-                         (with-label ,y (set! r2 0) (jump ,z))
-                         (with-label ,z (halt r0)))))))
+ (check-equal? (flatten-begins '(halt 1)) '(begin (halt 1)))
+ (check-equal? (flatten-begins '(begin (set! rbx 1) (halt rbx))) '(begin (set! rbx 1) (halt rbx)))
+ (check-equal? (flatten-begins '(begin (set! rax 0) (begin (set! rbx 1) (halt rbx))))
+               '(begin (set! rax 0) (set! rbx 1) (halt rbx)))
+ (check-equal? (flatten-begins '(begin (begin (set! rax 0)) (halt rax))) '(begin (set! rax 0) (halt rax))))
