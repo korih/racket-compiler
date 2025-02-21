b57dd55956486add73e7f042f8705a4a9395dcb5
arun <arun.sfc@outlook.com>
2025-02-21 00:19:10 -0800

    create all the tests and start with implementation

--- passes/optimize-predicates.rkt
+++ passes/optimize-predicates.rkt
@@ -2,15 +2,69 @@
 
 (provide optimize-predicates)
 
+(require "common.rkt")
+
 (require
- cpsc411/langs/v4)
+  cpsc411/compiler-lib
+  cpsc411/langs/v4)
 
 ;; Exercise 11
 ;; nested-asm-lang-v4 -> nested-asm-lang-v4
 ;; simplifies the predicates in the program
 (define/contract (optimize-predicates p)
   (-> nested-asm-lang-v4? nested-asm-lang-v4?)
-  (void))
+  ;; (Envof nested-asm-lang-v4-loc RangeValue)
+  ;; invariant: env contains a mapping of the locations to their currently known value ranges
+  (define env empty-env)
+
+  ;; nested-asm-lang-v4-tail -> nested-asm-lang-v4-tail
+  ;; interp. optimize predicates in the program tail
+  (define (optimize-predicates/tail t)
+    (match t
+      [`(halt ,triv) `(halt ,triv)]
+      [`(begin ,fx ... ,tail) `(begin ,@(map optimize-predicates/effect fx) ,(optimize-predicates/tail tail))]
+      [`(if ,pred ,t-tail ,f-tail) (optimize-conditional pred
+                                                     (optimize-predicates/tail t-tail)
+                                                     (optimize-predicates/tail f-tail))]))
+
+  ;; nested-asm-lang-v4-pred nested-asm-lang-v4-tail nested-asm-lang-v4-tail -> nested-asm-lang-v4-tail
+  ;; interp. optimize the predicate if possible and return the corresponding tail
+  (define (optimize-conditional pred k-t k-f)
+    (match pred
+      [`(,relop ,loc ,triv) (void)]
+      ['(true) k-t]
+      ['(false) k-f]
+      [`(not ,pred) (optimize-conditional pred k-f k-t)]
+      [`(begin ,fx ... ,pred)
+       `(begin ,@(map optimize-predicates/effect fx) ,(optimize-conditional pred k-t k-f))]
+      [`(if ,pred ,t-pred ,f-pred)
+       (optimize-conditional pred
+                             (optimize-conditional t-pred k-t k-f)
+                             (optimize-conditional f-pred k-t k-f))]))
+
+  ;; nested-asm-lang-v4-effect -> nested-asm-lang-v4-effect
+  ;; interp. optimize predicates in the program effect
+  (define (optimize-predicates/effect e)
+    (match e
+      [`(set! ,loc ,triv) (set! env (extend-env env loc (interp-triv triv)))
+                          `(set! ,loc ,triv)]
+      [`(set! ,loc (,binop ,loc ,triv))
+       (define triv-val (interp-triv triv))
+       (set! env (extend-env env loc (interp-binop binop (lookup-env env loc) triv-val)))
+       `(set! ,loc ,triv-val)]
+      [`(begin ,fx ...) `(begin ,@(map optimize-predicates/effect fx))]
+      [`(if ,pred ,t-e ,f-e) (void)]))
+
+  ;; nested-asm-lang-v4-binop RangeValue RangeValue -> RangeValue
+  ;; interp. the known abstract value resulting from the binary operation
+  (define (interp-binop binop v1 v2) (void))
+
+  ;; nested-asm-lang-v4-triv -> RangeValue
+  ;; interp. the known value of the triv
+  (define (interp-triv triv) (void))
+
+  (match p
+    [`(module ,tail) (optimize-predicates/tail tail)]))
 
 (module+ test
   (require rackunit)
@@ -23,4 +77,15 @@
   (check-equal? (optimize-predicates '(module (if (if (true) (false) (true)) (halt 0) (halt 1))))
                 '(module (halt 1)))
   (check-equal? (optimize-predicates '(module (begin (set! rax 1) (if (> rax 0) (halt 0) (halt 1)))))
-                '(module (begin (set! rax 1) (halt 0)))))
+                '(module (begin (set! rax 1) (halt 0))))
+  (check-equal? (optimize-predicates '(module (begin (set! rbx 1) (if (< rbx 1) (halt 0) (halt 3)))))
+                '(module (begin (set! rbx 1) (halt 3))))
+  (check-equal? (optimize-predicates `(module (begin (set! rbx rax) (if (<= rbx ,(max-int 64)) (halt 12) (halt 133)))))
+                '(module (begin (set! rbx rax) (halt 12))))
+  (check-equal? (optimize-predicates '(module (begin (set! rbx rax)
+                                                     (if (= rbx rax)
+                                                         (begin (set! rax rcx) (halt rax))
+                                                         (halt rax)))))
+                '(module (begin (set! rbx rax) (begin (set! rax rcx) (halt rax)))))
+  (check-equal? (optimize-predicates '(module (begin (set! rbx 33) (if (= rbx 44) (halt -1) (halt 0)))))
+                '(module (begin (set! rbx 33) (halt 0)))))
