6fdb1dc6bd1ddff265a27db869c1963a8855efc4
Lance Tan <ltan0521@gmail.com>
2025-02-27 20:29:16 -0800

    Clean up undead-analysis

--- passes/undead-analysis.rkt
+++ passes/undead-analysis.rkt
@@ -13,15 +13,12 @@
 (define/contract (undead-analysis p)
   (-> asm-pred-lang-v5/locals? asm-pred-lang-v5/undead?)
 
-  ;; (asm-pred-lang-v4/locals tail) -> undead-set-tree undead-out-set
-  ;; go through the tail and analyze the undead sets
+  ;; asm-pred-lang-v5/locals.tail -> (values undead-set-tree undead-set)
   (define (analyze-tail t)
     (match t
       [`(begin ,effects ... ,tail)
-
        (define-values (t-ust undead-out)
          (analyze-tail tail))
-
        (define-values (ust undead-in)
          (for/foldr ([rev-ust (list t-ust)]
                      [undead-out undead-out])
@@ -30,28 +27,18 @@
              (analyze-effects effect undead-out))
            (values (cons ust rev-ust) undead-in)))
        (values ust undead-in)]
-
       [`(if ,pred ,t1 ,t2)
        (define-values (t1-ust t1-undead-out) (analyze-tail t1))
        (define-values (t2-ust t2-undead-out) (analyze-tail t2))
-
-       ;; union the set of undead-outs
        (define tail-undead-in (set-union t1-undead-out t2-undead-out))
-
-       ;; cons new ust to ust
        (define ust^ (list t1-ust t2-ust))
-
-       ;; analyze pred might need to take undead-in
        (define-values (p-ust p-undead-out) (analyze-pred pred tail-undead-in))
-
        (values (cons p-ust ust^) p-undead-out)]
+      [`(halt ,triv)
+       (define undead-in (analyze-triv triv))
+       (values '() undead-in)]))
 
-      [`(halt ,triv) (define undead-in
-                       (analyze-triv triv))
-                     (values '() undead-in)]))
-
-  ;; (asm-pred-lang-v4/locals effects) undead-in-set -> undead-set-tree undead-out-set
-  ;; look through effects and create a undead-set-tree and undead-in-set for current effect
+  ;; asm-pred-lang-v5/locals.effect undead-set -> (values undead-set-tree undead-set)
   (define (analyze-effects e undead-out)
     (match e
       [`(begin ,effects ...)
@@ -78,16 +65,12 @@
       [`(if ,pred ,e1 ,e2)
        (define-values (e1-ust e1-undead-out) (analyze-effects e1 undead-out))
        (define-values (e2-ust e2-undead-out) (analyze-effects e2 undead-out))
-
        (define effect-undead-in (set-union e1-undead-out e2-undead-out))
        (define ust (list e1-ust e2-ust))
-
        (define-values (p-ust p-undead-out) (analyze-pred pred effect-undead-in))
-
        (values (cons p-ust ust) p-undead-out)]))
 
-  ;; (asm-pred-lang-v4/locals pred) undead-in-set -> undead-set-tree undead-out-set
-  ;; go through the pred and analyze the undead sets
+  ;; asm-pred-lang-v5/locals.pred undead-set -> (values undead-set-tree undead-set)
   (define (analyze-pred p undead-out)
     (match p
       [`(not ,pred)
@@ -96,7 +79,6 @@
       [`(begin ,effects ... ,pred)
        (define-values (p-out p-undead-out)
          (analyze-pred pred undead-out))
-
        (define-values (e-ust e-undead-out)
          (for/foldr ([ust^ (list p-out)]
                      [undead-in^ p-undead-out])
@@ -105,23 +87,18 @@
              (analyze-effects effect undead-in^))
            (values (cons ust ust^) undead-in)))
        (values e-ust e-undead-out)]
-
       [`(if ,p1 ,p2 ,p3)
        (define-values (p3-ust p3-undead-in) (analyze-pred p3 undead-out))
        (define-values (p2-ust p2-undead-in) (analyze-pred p2 undead-out))
-
        (define pred-undead-in (set-union p3-undead-in p2-undead-in))
        (define ust (list p3-ust p2-ust))
-
        (define-values (p1-ust p1-undead-out) (analyze-pred p1 pred-undead-in))
-       (values (cons p1-ust ust) p1-undead-out)] ; just call pred for each one then combine?
-
+       (values (cons p1-ust ust) p1-undead-out)]
       [`(,relop ,aloc ,triv)
        (define undead-in (set-union (set-add undead-out aloc) (analyze-triv triv)))
        (values undead-out undead-in)]
-
-      ;; catch case where pred is true or false
-      [_ (values undead-out undead-out)]))
+      ['(true) (values undead-out undead-out)]
+      ['(false) (values undead-out undead-out)]))
 
   ;; (asm-pred-lang-v4/locals triv) -> (Listof aloc)
   ;; analyze the triv value, if its an aloc return it as a list
@@ -131,18 +108,11 @@
       [x #:when (aloc? x) (list x)]
       [_ '()]))
 
-  ;; info (asm-pred-lang-v4/locals tail) -> (asm-pred-lang-v4/undead info)
-  ;; analyze the tail and add the undead set tree to the info
-  (define (compile-info i tail)
-    (match i
-      [`,info
-       (define-values (ust^ _)
-         (analyze-tail tail))
-       (info-set info 'undead-out ust^)]))
-
   (match p
     [`(module ,info ,tail)
-     `(module ,(compile-info info tail) ,tail)]))
+     (define-values (undead-tree _) (analyze-tail tail))
+     (define updated-info (info-set info 'undead-out undead-tree))
+     `(module ,updated-info ,tail)]))
 
 (module+ test
 
