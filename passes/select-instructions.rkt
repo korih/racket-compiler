afa29cfb6d8eaecc00aac2ee2fd71f213bb4a5f2
UBC Student <kori0909@students.cs.ubc.ca>
2025-02-28 22:55:53 -0800

    Select-instructions (#10)
    * select-instructions
    
    * Squash merge milestone-5 into kori-m5
    
    ---------
    
    Co-authored-by: korih <96402323+korih@users.noreply.github.com>

--- passes/select-instructions.rkt
+++ passes/select-instructions.rkt
@@ -2,7 +2,7 @@
 
 (require
   cpsc411/compiler-lib
-  cpsc411/langs/v4
+  cpsc411/langs/v5
   rackunit)
 
 (provide select-instructions)
@@ -12,14 +12,7 @@
 ;; compiles p to Asm-pred-lang v4 by selecting appropriate sequences of abstract
 ;; assembly instructions to implement the operations of the source language
 (define/contract (select-instructions p)
-  (-> imp-cmf-lang-v4? asm-pred-lang-v4?)
-
-  ;; imp-cmf-lang-v4.tail -> bool
-  ;; produces true if the expression is a valid value in tail position
-  (define (tail-value? e)
-    (match e
-      [`(begin ,fx ... ,tail) #f]
-      [_ #t]))
+  (-> imp-cmf-lang-v5? asm-pred-lang-v5?)
 
   ; imp-cmf-lang-v4.value -> (list (List-of asm-pred-lang-v4.effect) aloc)
   ; Assigns the value v to a fresh temporary, returning two values: the list of
@@ -36,6 +29,7 @@
   ;; imp-cmf-lang-v4.tail -> asm-pred-lang-v4.tail
   (define (select-tail e)
     (match e
+      [`(jump ,trg ,loc ...) `(jump ,trg ,@loc)]
       [`(if ,pred ,tail1 ,tail2)
        `(if ,(select-pred pred)
             ,(select-tail tail1)
@@ -45,27 +39,25 @@
                              ([e fx])
                              (append (select-effect e) instructions)))
        (define tail-compiled (select-tail tail))
-       (match tail-compiled
-         [`(begin ,inner-compiled-fx ... ,inner-compiled-tail)
-          #:when (tail-value? tail)
-          `(begin ,@compiled-fx ,@inner-compiled-fx ,inner-compiled-tail)]
-         [_ `(begin ,@compiled-fx ,tail-compiled)])]
-      [value (match-let ([`(,stmts ,loc) (select-value value)])
-               (if (empty? stmts)
-                   `(halt ,loc)
-                   `(begin ,@stmts (halt ,loc))))]))
+       (cond
+         [(empty? compiled-fx) tail-compiled]
+         [else (match tail-compiled
+                 [`(begin ,inner-effects^ ... ,inner-tail^)
+                  `(begin ,@compiled-fx ,@inner-effects^ ,@inner-tail^)]
+                 [_ `(begin ,@compiled-fx ,tail-compiled)])])]
+
+      [value (select-value value)]))
 
   ;; imp-cmf-lang-v4.value -> (list (List-of asm-pred-lang-v4.effect) aloc)
   (define (select-value e)
     (match e
       [`(,binop ,op1 ,op2)
        (define op1-tmp (fresh 'tmp))
-       (define op2-tmp (fresh 'tmp))
-       (list (list `(set! ,op1-tmp ,op1)
-                   `(set! ,op2-tmp ,op2)
-                   `(set! ,op1-tmp (,binop ,op1-tmp ,op2-tmp)))
-             op1-tmp)]
-      [triv (select-triv triv)]))
+       `(begin 
+          (set! ,op1-tmp ,op1)
+          (set! ,op1-tmp (,binop ,op1-tmp ,op2))
+          (halt ,op1-tmp))]
+      [triv `(halt ,triv)]))
 
   ;; aloc imp-cmf-lang-v4.effect -> (List-of asm-pred-lang-v4.effect)
   (define (convert-set-expr x v)
@@ -91,12 +83,13 @@
                   ,@(select-effect e2)))]))
 
   ;; imp-cmf-lang-v4.triv -> (list (List-of asm-pred-lang-v4.effect) aloc)
+  ;; opand -> int or loc and label
   (define (select-triv t)
     (match t
-      [x #:when (aloc? x) (list empty x)]
       [x #:when (int64? x)
-       (define tmp (fresh 'tmp))
-       (list (list `(set! ,tmp ,x)) tmp)]))
+         (define tmp (fresh 'tmp))
+         (list (list `(set! ,tmp ,x)) tmp)]
+      [x (list empty x)]))
 
   ;; imp-cmf-lang-v4.pred -> asm-pred-lang-v4.pred
   (define (select-pred p)
@@ -118,11 +111,44 @@
       ['(true) p]
       ['(false) p]))
 
+  (define (select-func f)
+    (match f
+      [`(define ,label ,tail) `(define ,label () ,(select-tail tail))]))
+
   (match p
-    [`(module ,tail)
-     `(module () ,(select-tail tail))]))
+    [`(module ,funcs ... ,tail)
+     `(module () ,@(map select-func funcs) ,(select-tail tail))]))
 
 (module+ test
+  (check-equal? (select-instructions '(module (+ 2 2)))
+                '(module () (begin (set! tmp.1 2) (set! tmp.1 (+ tmp.1 2)) (halt tmp.1))))
+  (check-equal? (select-instructions '(module (define L.start.1 1) (begin (set! x.1 5) x.1)))
+                '(module () (define L.start.1 () (halt 1)) (begin (set! x.1 5) (halt x.1))))
+  (check-equal? (select-instructions '(module (begin (begin (begin 5)))))
+                '(module () (halt 5)))
+  (check-equal? (select-instructions
+                 '(module
+                      (define L.start.1 (begin
+                                          (set! x.1 0)
+                                          (if
+                                           (if (> rax rbx)
+                                               (< 1 fv1)
+                                               (> x.1 5))
+                                           (set! x.1 1)
+                                           (set! x.1 2))
+                                          x.1))
+                    (jump L.start.1 x.1)))
+                '(module
+                     ()
+                   (define L.start.1
+                     ()
+                     (begin
+                       (set! x.1 0)
+                       (if (if (> rax rbx) (begin (set! tmp.2 1) (< tmp.2 fv1)) (> x.1 5))
+                           (set! x.1 1)
+                           (set! x.1 2))
+                       (halt x.1)))
+                   (jump L.start.1 x.1)))
   (check-equal? (select-instructions '(module (begin (set! x.1 5) x.1)))
                 '(module () (begin (set! x.1 5) (halt x.1))))
   (check-equal? (select-instructions '(module (begin (set! x.1 (+ 2 2)) x.1)))
@@ -133,8 +159,6 @@
                 '(module () (begin (set! x.1 2) (set! x.1 (+ x.1 x.1)) (halt x.1))))
   (check-equal? (select-instructions '(module (begin (set! x.1 2) (begin (set! x.1 (+ x.1 2))) x.1)))
                 '(module () (begin (set! x.1 2) (set! x.1 (+ x.1 2)) (halt x.1))))
-  (check-equal? (select-instructions '(module (begin (begin (begin 5)))))
-                '(module () (begin (begin (begin (set! tmp.1 5) (halt tmp.1))))))
   (check-equal? (select-instructions '(module (begin
                                                 (begin
                                                   (set! x.1 1)
@@ -150,10 +174,9 @@
                 '(module
                      ()
                    (begin
-                     (begin
-                       (set! x.1 1)
-                       (set! x.2 2)
-                       (if (begin (set! x.2 (+ x.2 1)) (= x.2 3))
-                           (set! x.1 (+ x.1 x.2))
-                           (set! x.1 (* x.1 x.2)))
-                       (if (if (not (> x.1 1)) (false) (true)) (halt x.1) (halt x.2)))))))
+                     (set! x.1 1)
+                     (set! x.2 2)
+                     (if (begin (set! x.2 (+ x.2 1)) (= x.2 3))
+                         (set! x.1 (+ x.1 x.2))
+                         (set! x.1 (* x.1 x.2)))
+                     (if (if (not (> x.1 1)) (false) (true)) (halt x.1) (halt x.2))))))
