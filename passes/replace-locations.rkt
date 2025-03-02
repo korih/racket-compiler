86c97984936cb2e14acc3975fb7e7e1649ea5875
Lance Tan <ltan0521@gmail.com>
2025-03-02 14:30:29 -0800

    Fix failing tests for replace-locations

--- passes/replace-locations.rkt
+++ passes/replace-locations.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "common.rkt")
+
 (require
   cpsc411/compiler-lib
   cpsc411/langs/v5
@@ -8,16 +10,29 @@
 (provide replace-locations)
 
 ;; asm-lang-v5/assignments -> nested-asm-lang-v5
-;; interp. replaces the abstract locations with the concrete locations
+;; compiles p to Nested-asm-lang v5 by replacing all abstract location with
+;; physical locations using the assignment described in the assignment info
+;; field, and dropping any register-allocation-related metadata from the program
 (define (replace-locations p)
   (-> asm-pred-lang-v5/assignments? nested-asm-lang-v5?)
 
-  ;; asm-lang-v5/assignments.tail (dictof asm-lang-v5/assignments.aloc asm-lang-v5/assignments.rloc)
-  ;; -> nested-asm-lang-v5.tail
+  ;; asm-lang-v5/assignments -> (Dict-of aloc rloc)
+  ;; interp. creates a dictionary of assignments
+  (define (make-assignments-dict assignments)
+    (for/fold ([acc (hash)])
+              ([pair assignments])
+      (dict-set acc (first pair) (second pair))))
+
+  ;; asm-pred-lang-v5/assignments.label asm-pred-lang-v5/assignments.info asm-pred-lang-v5/assignments.tail -> partial nested-asm-lang-v5/assignments.p
+  (define (replace-locations-func label info tail)
+    (define assignments (make-assignments-dict (info-ref info 'assignment)))
+    `(define ,label ,(replace-locations-tail tail assignments)))
+
+  ;; asm-lang-v5/assignments.tail (Dict-of aloc rloc) -> nested-asm-lang-v5.tail
   (define (replace-locations-tail t assignments)
     (match t
-      [`(halt ,triv)
-       `(halt ,(replace-locations-triv triv assignments))]
+      [`(halt ,op)
+       `(halt ,(replace-locations-opand op assignments))]
       [`(begin ,fx ... ,tail)
        (define compiled-fx (for/list ([e fx])
                              (replace-locations-effect e assignments)))
@@ -28,34 +43,20 @@
        (define t1^ (replace-locations-tail t1 assignments))
        (define t2^ (replace-locations-tail t2 assignments))
        `(if ,pred^ ,t1^ ,t2^)]
-      [`(jump ,trg ,loc ...)
-       `(jump ,(replace-locations-trg trg assignments)
-              ,@(for/list ([l loc]) (replace-locations-triv l assignments)))]))
+      [`(jump ,trg ,locs ...)
+       `(jump ,(replace-locations-trg trg assignments))]))
 
-  ;; asm-pred-lang-v5/assignments.trg (dictof asm-lang-v5/assignments.aloc asm-lang-v5/assignments.rloc)
-  ;; -> nested-asm-lang-v5.trg
-  ;; interp. replaces abstract location if trg is a label
-  (define (replace-locations-trg trg assignments)
-    (match trg
-      [l #:when (label? l) l]
-      [loc (replace-locations-loc loc assignments)]))
-
-  ;; asm-pred-lang-v5/assignments.log (dictof asm-lang-v5/assignments.aloc asm-lang-v5/assignments.rloc)
-  ;; -> nested-asm-lang-v5.log
-  (define (replace-locations-loc loc assignments)
-    (match loc
-      [a #:when (aloc? a) (dict-ref assignments a)]
-      [loc loc]))
-
-  ;; asm-lang-v5/assignments.effect (dictof asm-lang-v5/assignments.aloc asm-lang-v5/assignments.rloc)
-  ;; -> nested-asm-lang-v5.effect
+  ;; asm-lang-v5/assignments.effect (Dict-of aloc rloc) -> nested-asm-lang-v5.effect
   (define (replace-locations-effect e assignments)
     (match e
-      [`(set! ,x (,binop ,x ,v))
-       (define reg (dict-ref assignments x))
-       `(set! ,reg (,binop ,reg ,(replace-locations-triv v assignments)))]
-      [`(set! ,x ,v)
-       `(set! ,(dict-ref assignments x) ,(replace-locations-triv v assignments))]
+      [`(set! ,loc (,binop ,loc ,op))
+       (define loc^ (replace-locations-loc loc assignments))
+       (define op^ (replace-locations-opand op assignments))
+       `(set! ,loc^ (,binop ,loc^ ,op^))]
+      [`(set! ,loc ,triv)
+       (define loc^ (replace-locations-loc loc assignments))
+       (define triv^ (replace-locations-triv triv assignments))
+       `(set! ,loc^ ,triv^)]
       [`(begin ,fx ... ,e)
        (define compiled-fx (for/list ([e fx]) (replace-locations-effect e assignments)))
        (define compiled-e (replace-locations-effect e assignments))
@@ -66,12 +67,11 @@
        (define e2^ (replace-locations-effect e2 assignments))
        `(if ,pred^ ,e1^ ,e2^)]))
 
-  ;; asm-lang-v5/assignments.pred (dictof asm-lang-v5/assignments.aloc asm-lang-v5/assignments.rloc)
-  ;; -> nested-asm-lang-v5.pred
+  ;; asm-lang-v5/assignments.pred (Dict-of aloc rloc) -> nested-asm-lang-v5.pred
   (define (replace-locations-pred p assignments)
     (match p
-      [`(true) `(true)]
-      [`(false) `(false)]
+      ['(true) p]
+      ['(false) p]
       [`(begin ,effects ... ,pred)
        (define effects^
          (for/list ([effect effects])
@@ -79,35 +79,39 @@
        (define pred^ (replace-locations-pred pred assignments))
        `(begin ,@effects^ ,pred^)]
       [`(not ,pred) `(not ,(replace-locations-pred pred assignments))]
-      [`(,relop ,aloc ,triv)
-       (define reg (dict-ref assignments aloc))
-       `(,relop ,reg ,triv)]
+      [`(,relop ,loc ,op)
+       (define loc^ (replace-locations-loc loc assignments))
+       (define op^ (replace-locations-opand op assignments))
+       `(,relop ,loc^ ,op^)]
       [`(if ,p1 ,p2 ,p3)
        (define p1^ (replace-locations-pred p1 assignments))
        (define p2^ (replace-locations-pred p2 assignments))
        (define p3^ (replace-locations-pred p3 assignments))
        `(if ,p1^ ,p2^ ,p3^)]))
 
-  ;; asm-lang-v5/assignments.triv (dictof asm-lang-v5/assignments.aloc asm-lang-v5/assignments.rloc)
-  ;; -> nested-asm-lang-v5.effect
+  ;; asm-lang-v5/assignments.triv (Dict-of aloc rloc) -> nested-asm-lang-v5.triv
   (define (replace-locations-triv t assignments)
     (match t
-      [`,x #:when (aloc? x) (dict-ref assignments x)]
-      [`,x x]))
+      [label #:when (label? label) label]
+      [op (replace-locations-opand op assignments)]))
 
-  ;; asm-lang-v5/assignments -> (dictof asm-lang-v5/assignments.aloc asm-lang-v5/assignments.rloc)
-  ;; interp. creates a dictionary of assignments
-  (define (make-assignments-dict assignments)
-    (for/fold ([acc (hash)])
-              ([pair assignments])
-      (dict-set acc (first pair) (second pair))))
+  ;; asm-lang-v5/assignments.opand (Dict-of aloc rloc) -> nested-asm-lang-v5.opand
+  (define (replace-locations-opand op assignments)
+    (match op
+      [int64 #:when (int64? int64) int64]
+      [loc (replace-locations-loc loc assignments)]))
 
-  ;; asm-pred-lang-v5/assignments.label asm-pred-lang-v5/assignments.info asm-pred-lang-v5/assignments.tail ->
-  ;; partial nested-asm-lang-v5/assignments.p
-  ;; interp. replaces abstract locations within the scope of the label
-  (define (replace-locations-func label info tail)
-    (define assignments (make-assignments-dict (info-ref info 'assignment)))
-    `(define ,label ,(replace-locations-tail tail assignments)))
+  ;; asm-lang-v5/assignments.loc (Dict-of aloc rloc) -> nested-asm-lang-v5.loc
+  (define (replace-locations-loc loc assignments)
+    (match loc
+      [aloc #:when (aloc? aloc) (dict-ref assignments aloc)]
+      [rloc #:when (rloc? rloc) rloc]))
+
+  ;; asm-lang-v5/assignments.trg (Dict-of aloc rloc) -> nested-asm-lang-v5.trg
+  (define (replace-locations-trg trg assignments)
+    (match trg
+      [label #:when (label? label) label]
+      [loc (replace-locations-loc loc assignments)]))
 
   (match p
     [`(module ,info (define ,labels ,infos ,tails) ... ,tail)
@@ -116,6 +120,18 @@
         ,(replace-locations-tail tail (make-assignments-dict (info-ref info 'assignment))))]))
 
 (module+ test
+  (check-equal? (replace-locations '(module ((locals ()) (assignment ())) (define L.id.13 ((locals (x.2)) (assignment ((x.2 rsp)))) (begin (set! x.2 rdi) (halt x.2))) (begin (set! rdi 5) (jump L.id.13 rbp rdi))))
+                '(module
+                     (define L.id.13 (begin (set! rsp rdi) (halt rsp)))
+                   (begin (set! rdi 5) (jump L.id.13))))
+  (check-equal? (replace-locations '(module ((locals (x.1 y.2)) (assignment ((y.2 rsp) (x.1 rsp)))) (begin (set! x.1 3) (set! y.2 x.1) (if (> y.2 x.1) (halt x.1) (halt y.2)))))
+                '(module
+                     (begin (set! rsp 3) (set! rsp rsp) (if (> rsp rsp) (halt rsp) (halt rsp)))))
+  (check-equal? (replace-locations '(module ((locals (tmp.18 y.6)) (assignment ((y.6 rbx) (tmp.18 rsp)))) (begin (set! y.6 200) (if (begin (set! tmp.18 3) (< tmp.18 y.6)) (halt 1) (halt 0)))))
+                '(module
+                     (begin
+                       (set! rbx 200)
+                       (if (begin (set! rsp 3) (< rsp rbx)) (halt 1) (halt 0)))))
   (check-equal? (replace-locations '(module ((locals (x.1)) (assignment ((x.1 rax))))
                                       (begin (set! x.1 1) (if (> x.1 0) (set! x.1 2) (set! x.1 3)) (halt x.1))))
                 '(module
@@ -188,7 +204,7 @@
                                       (begin (set! x.1 0) (jump ,label2 x.1))))
                 `(module
                      (define ,label2 (begin (set! r9 (+ r9 1)) (halt r9)))
-                   (begin (set! r8 0) (jump ,label2 r8))))
+                   (begin (set! r8 0) (jump ,label2))))
 
   (define label1 (fresh-label))
   (define label3 (fresh-label))
@@ -211,7 +227,7 @@
                    (define ,label3 (begin (set! r8 -1)
                                           (set! r8 (+ r8 2))
                                           (halt r8)))
-                   (begin (set! r7 1) (jump ,label1 r7))))
+                   (begin (set! r7 1) (jump ,label1))))
 
   (define label4 (fresh-label))
   (define label5 (fresh-label))
@@ -228,13 +244,13 @@
                                              (set! x.3 x.2)
                                              (jump ,label4 x.3))))
                 `(module (define ,label4 (begin (set! r1 (+ r1 r2))
-                                               (jump ,label5 r1 r2)))
+                                               (jump ,label5)))
                    (define ,label5 (begin (set! r2 (* r2 fv0)) (halt r2)))
                    (begin (set! rax 1)
                           (set! rbx rax)
                           (set! rbx (+ rbx rax))
                           (set! rcx rbx)
-                          (jump ,label4 rcx))))
+                          (jump ,label4))))
 
     (define label6 (fresh-label))
     (define label7 (fresh-label))
@@ -249,11 +265,11 @@
                                           (set! x.6 x.5)
                                           (set! x.6 (* x.6 x.5))
                                           (jump ,label6 x.6))))
-                  `(module (define ,label6 (jump ,label7 r1))
-                     (define ,label7 (jump ,label6 r2))
+                  `(module (define ,label6 (jump ,label7))
+                     (define ,label7 (jump ,label6))
                      (begin (set! fv0 0)
                        (set! fv1 3)
                        (set! fv1 (+ fv1 fv0))
                        (set! r0 fv1)
                        (set! r0 (* r0 fv1))
-                       (jump ,label6 r0)))))
+                       (jump ,label6)))))
