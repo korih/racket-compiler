7b0356ea85ea121c0f28da6771fe11b604dc7bfd
korih <96402323+korih@users.noreply.github.com>
2025-04-04 12:47:36 -0700

    define-let rec

--- passes/define-letrec.rkt
+++ passes/define-letrec.rkt
@@ -13,6 +13,86 @@
 (define/contract (define->letrec p)
   (-> exprs-unsafe-lang-v9? just-exprs-lang-v9?)
 
-  (void))
+  (define (transform-funcs->let f)
+    (match f
+      [`(define ,label (lambda (,alocs ...) ,value))
+       `(,label (lambda ,alocs ,value))]))
 
-(module+ test)
+  (define (transform-funcs->letrec funcs v)
+    (cond
+      [(empty? funcs) v]
+      [else
+       `(letrec ,funcs ,v)]))
+
+  (match p
+    [`(module ,funcs ... ,value)
+     (define funcs^ (for/fold ([acc '()])
+                              ([f funcs])
+                      (cons (transform-funcs->let f) acc)))
+     `(module ,(transform-funcs->letrec (reverse funcs^) value))]))
+
+(module+ test
+  (check-equal?
+   (define->letrec '(module
+                        (define |+.2|
+                          (lambda (tmp.3 tmp.4) (unsafe-procedure-call |+.1| tmp.3 tmp.4)))
+                      (define |+.1|
+                        (lambda (tmp.1 tmp.2)
+                          (if (fixnum? tmp.1)
+                              (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2) (error 2))
+                              (error 2))))
+                      (unsafe-procedure-call |+.2| 1 2)))
+   '(module
+        (letrec ((|+.2|
+                  (lambda (tmp.3 tmp.4) (unsafe-procedure-call |+.1| tmp.3 tmp.4)))
+                 (|+.1|
+                  (lambda (tmp.1 tmp.2)
+                    (if (fixnum? tmp.1)
+                        (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2) (error 2))
+                        (error 2)))))
+          (unsafe-procedure-call |+.2| 1 2)))
+   "check basic define->letrec transformation")
+
+  (check-equal?
+   (define->letrec '(module
+                        (define |+.1|
+                          (lambda (tmp.1 tmp.2)
+                            (if (fixnum? tmp.1)
+                                (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2) (error 2))
+                                (error 2))))
+                      (define |+.2|
+                        (lambda (tmp.3 tmp.4) (unsafe-procedure-call |+.1| tmp.3 tmp.4)))
+                      (unsafe-procedure-call |+.2| 1 2)))
+   '(module
+        (letrec ((|+.1|
+                  (lambda (tmp.1 tmp.2)
+                    (if (fixnum? tmp.1)
+                        (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2) (error 2))
+                        (error 2))))
+                 (|+.2|
+                  (lambda (tmp.3 tmp.4) (unsafe-procedure-call |+.1| tmp.3 tmp.4))))
+          (unsafe-procedure-call |+.2| 1 2)))
+   "check functions that depend on each other")
+
+  (check-equal?
+   (define->letrec '(module
+                        (define |+.1|
+                          (lambda (tmp.1 tmp.2)
+                            (if (fixnum? tmp.1)
+                                (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2) (error 2))
+                                (error 2))))
+                      (error 42)))
+   '(module
+        (letrec ((|+.1|
+                  (lambda (tmp.1 tmp.2)
+                    (if (fixnum? tmp.1)
+                        (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2) (error 2))
+                        (error 2)))))
+          (error 42)))
+   "check wrong-lambda arity")
+  (check-equal?
+   (define->letrec '(module (error 42)))
+   '(module (error 42))
+   "Check no functions")
+
+  )
