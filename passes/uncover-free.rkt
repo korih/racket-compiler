3dada06d3187e85bccafe80ba389df4acb4ecab6
korih <96402323+korih@users.noreply.github.com>
2025-04-06 11:59:10 -0700

    should be done?

--- passes/uncover-free.rkt
+++ passes/uncover-free.rkt
@@ -78,7 +78,7 @@
     (match v
       [`(begin ,effects ... ,value) (define-values (effects^ free-var-effects) (traverse-effects-list effects env))
                                     (define-values (value^ free-vars-values) (uncover-free-value value env))
-                                    (values `(begin ,@effects^ ,@value^) (append free-var-effects free-vars-values))]
+                                    (values `(begin ,@effects^ ,value^) (append free-var-effects free-vars-values))]
       [`(if ,v1 ,v2 ,v3) (define-values (v1^ free-vars1) (uncover-free-value v1 env))
                          (define-values (v2^ free-vars2) (uncover-free-value v2 env))
                          (define-values (v3^ free-vars3) (uncover-free-value v3 env))
@@ -86,11 +86,12 @@
       [`(let ([,alocs ,vs] ...) ,body) (define-values (bindings free-var-binding) (traverse-bindings-list alocs vs env))
                                        (define env^ (extend-env* env alocs vs))
                                        (define-values (body^ free-vars) (uncover-free-value body env^))
-                                       (values `(let ,bindings',body^) (append free-vars free-var-binding))]
+                                       (values `(let ,bindings ,body^) (append free-vars free-var-binding))]
       [`(letrec ([,alocs ,vs] ...) ,body) (define-values (bindings free-var-binding) (traverse-letrec-bindings alocs vs env))
                                           (define env^ (extend-env* env alocs vs))
                                           (define-values (body^ free-vars) (uncover-free-value body env^))
-                                          (values `(letrec ,bindings  ,body^) (remove* free-var-binding free-vars))]
+                                          (values `(letrec ,bindings  ,body^)
+                                                  (remove* alocs (append free-var-binding free-vars) ))]
       [`(unsafe-procedure-call ,vs ...) (define-values (vs^ free-vars) (traverse-values-list vs env))
                                         (values `(unsafe-procedure-call ,@vs^) free-vars)]
       [`(,primops ,vs ...) (define-values (vs^ free-vars) (traverse-values-list vs env))
@@ -145,6 +146,25 @@
    "Book test: unsafe call with free")
 
   (check-equal?
+   (uncover-free
+    `(module
+         (letrec ([f.1 (lambda ()
+                         (letrec ([x.1 (lambda () (unsafe-procedure-call x.1 tmp.1))])
+                           x.1))])
+           f.1)))
+   '(module
+        (letrec ((f.1
+                  (lambda ((free (tmp.1)))
+                    ()
+                    (letrec ((x.1
+                              (lambda ((free (tmp.1 x.1)))
+                                ()
+                                (unsafe-procedure-call x.1 tmp.1))))
+                      x.1))))
+          f.1))
+   "Test with non-bounded variable in child letrec with no binding in letrec")
+
+  (check-equal?
    (uncover-free '(module
                       (letrec ((|+.1|
                                 (lambda (tmp.1 tmp.2)
@@ -167,5 +187,81 @@
                     (unsafe-procedure-call |+.1| tmp.3 tmp.4))))
           (unsafe-procedure-call |+.2| 1 2)))
    "Test with mutually dependent procedures")
+
+  (check-equal?
+   (uncover-free '(module
+                      (letrec ((|+.1|
+                                (lambda (tmp.1 tmp.2)
+                                  (if (fixnum? tmp.1)
+                                      (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2 tmp.3) (error 2))
+                                      (error 2))))
+                               (|+.2|
+                                (lambda (tmp.3 tmp.4) (unsafe-procedure-call |+.1| tmp.3 tmp.4))))
+                        (unsafe-procedure-call |+.2| 1 2))))
+   '(module
+        (letrec ((|+.1|
+                  (lambda ((free (tmp.3)))
+                    (tmp.1 tmp.2)
+                    (if (fixnum? tmp.1)
+                        (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2 tmp.3) (error 2))
+                        (error 2))))
+                 (|+.2|
+                  (lambda ((free (|+.1|)))
+                    (tmp.3 tmp.4)
+                    (unsafe-procedure-call |+.1| tmp.3 tmp.4))))
+          (unsafe-procedure-call |+.2| 1 2)))
+   "Test with procedure call with free variables not in lambda arg")
+
+  (check-equal?
+   (uncover-free '(module
+                      (letrec ((|+.1|
+                                (lambda (tmp.1 tmp.2)
+                                  (if (fixnum? tmp.1)
+                                      (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2 tmp.3) (error 2))
+                                      (error 2))))
+                               (|+.2|
+                                (lambda (tmp.3 tmp.4) (unsafe-procedure-call |+.1| tmp.3 tmp.4))))
+                        (unsafe-procedure-call |+.2| 1 2 tmp.3))))
+   '(module
+        (letrec ((|+.1|
+                  (lambda ((free (tmp.3)))
+                    (tmp.1 tmp.2)
+                    (if (fixnum? tmp.1)
+                        (if (fixnum? tmp.2) (unsafe-fx+ tmp.1 tmp.2 tmp.3) (error 2))
+                        (error 2))))
+                 (|+.2|
+                  (lambda ((free (|+.1|)))
+                    (tmp.3 tmp.4)
+                    (unsafe-procedure-call |+.1| tmp.3 tmp.4))))
+          (unsafe-procedure-call |+.2| 1 2 tmp.3)))
+   "Test with body cal with free variable")
+
+  (check-equal?
+   (uncover-free '(module
+                      (letrec ([a.1
+                                (lambda ()
+                                  (letrec ([a.2 (lambda ()
+                                                  (let ([a.3 (begin
+                                                               (eq? 1 2)
+                                                               (if (eq? 2 1)
+                                                                   tmp.3
+                                                                   tmp.4))])
+                                                    a.3))])
+                                    a.2))])
+                        a.1)))
+   '(module
+        (letrec ((a.1
+                  (lambda ((free (tmp.3 tmp.4)))
+                    ()
+                    (letrec ((a.2
+                              (lambda ((free (tmp.3 tmp.4)))
+                                ()
+                                (let ((a.3
+                                       (begin (eq? 1 2) (if (eq? 2 1) tmp.3 tmp.4))))
+                                  a.3))))
+                      a.2))))
+          a.1))
+   "Test with super nested letrec with let")
+
 
   )
