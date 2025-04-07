703ad54afa29e7456c1a639e9f100f04774c053b
korih <96402323+korih@users.noreply.github.com>
2025-04-07 10:01:12 -0700

    bug fix

--- passes/convert-closures.rkt
+++ passes/convert-closures.rkt
@@ -250,4 +250,136 @@
                             identity.13
                             identity.13
                             identity.13)))
-                         (closure-call fact.14 fact.14 5)))))))
+                         (closure-call fact.14 fact.14 5))))))
+  (check-equal?
+   (convert-closures
+    '(module
+         (letrec
+             ((*.103
+               (lambda ((free ()))
+                 (tmp.104 tmp.105)
+                 (if (fixnum? tmp.104)
+                     (if (fixnum? tmp.105)
+                         (unsafe-fx* tmp.104 tmp.105)
+                         (error 1))
+                     (error 1))))
+              (|+.100|
+               (lambda ((free ()))
+                 (tmp.101 tmp.102)
+                 (if (fixnum? tmp.101)
+                     (if (fixnum? tmp.102)
+                         (unsafe-fx+ tmp.101 tmp.102)
+                         (error 2))
+                     (error 2))))
+              (|-.97|
+               (lambda ((free ()))
+                 (tmp.98 tmp.99)
+                 (if (fixnum? tmp.98)
+                     (if (fixnum? tmp.99)
+                         (unsafe-fx- tmp.98 tmp.99)
+                         (error 3))
+                     (error 3))))
+              (eq?.94 (lambda ((free ()))
+                        (tmp.95 tmp.96)
+                        (eq? tmp.95 tmp.96)))
+              (identity.13 (lambda ((free (eq?.94 |+.100| identity.13 |-.97|)))
+                             (x.15)
+                             (if (unsafe-procedure-call eq?.94 x.15 0)
+                                 0
+                                 (let ((y.16 (unsafe-procedure-call |-.97| x.15 1)))
+                                   (let ((x.17
+                                          (unsafe-procedure-call identity.13 y.16)))
+                                     (unsafe-procedure-call |+.100| 1 x.17))))))
+              (fact.14 (lambda
+                           ((free (eq?.94
+                                   identity.13
+                                   *.103
+                                   fact.14
+                                   |-.97|
+                                   identity.13
+                                   identity.13
+                                   identity.13)))
+                         (x.18)
+                         (let
+                             ((x.19 (unsafe-procedure-call identity.13 x.18))
+                              (y.20 (unsafe-procedure-call identity.13 0)))
+                           (if (unsafe-procedure-call eq?.94 x.19 y.20)
+                               (let ((z.21 (unsafe-procedure-call identity.13 1)))
+                                 z.21)
+                               (let ((n.22 (unsafe-procedure-call identity.13 1)))
+                                 (let ((z.23 (unsafe-procedure-call |-.97| x.19 n.22)))
+                                   (let ((y.24 (unsafe-procedure-call fact.14 z.23)))
+                                     (unsafe-procedure-call *.103 x.19 y.24)))))))))
+           (unsafe-procedure-call fact.14 5))))
+   '(module
+        (letrec ((L.*.103.7
+                  (lambda (c.4 tmp.104 tmp.105)
+                    (let ()
+                      (if (fixnum? tmp.104)
+                          (if (fixnum? tmp.105) (unsafe-fx* tmp.104 tmp.105) (error 1))
+                          (error 1)))))
+                 (L.+.100.8
+                  (lambda (c.5 tmp.101 tmp.102)
+                    (let ()
+                      (if (fixnum? tmp.101)
+                          (if (fixnum? tmp.102) (unsafe-fx+ tmp.101 tmp.102) (error 2))
+                          (error 2)))))
+                 (L.-.97.9
+                  (lambda (c.6 tmp.98 tmp.99)
+                    (let ()
+                      (if (fixnum? tmp.98)
+                          (if (fixnum? tmp.99) (unsafe-fx- tmp.98 tmp.99) (error 3))
+                          (error 3)))))
+                 (L.eq?.94.10
+                  (lambda (c.7 tmp.95 tmp.96) (let () (eq? tmp.95 tmp.96))))
+                 (L.identity.13.11
+                  (lambda (c.8 x.15)
+                    (let ((eq?.94 (closure-ref c.8 0))
+                          (|+.100| (closure-ref c.8 1))
+                          (identity.13 (closure-ref c.8 2))
+                          (|-.97| (closure-ref c.8 3)))
+                      (if (closure-call eq?.94 eq?.94 x.15 0)
+                          0
+                          (let ((y.16 (closure-call |-.97| |-.97| x.15 1)))
+                            (let ((x.17 (closure-call identity.13 identity.13 y.16)))
+                              (closure-call |+.100| |+.100| 1 x.17)))))))
+                 (L.fact.14.12
+                  (lambda (c.9 x.18)
+                    (let ((eq?.94 (closure-ref c.9 0))
+                          (identity.13 (closure-ref c.9 1))
+                          (*.103 (closure-ref c.9 2))
+                          (fact.14 (closure-ref c.9 3))
+                          (|-.97| (closure-ref c.9 4))
+                          (identity.13 (closure-ref c.9 5))
+                          (identity.13 (closure-ref c.9 6))
+                          (identity.13 (closure-ref c.9 7)))
+                      (let ((x.19 (closure-call identity.13 identity.13 x.18))
+                            (y.20 (closure-call identity.13 identity.13 0)))
+                        (if (closure-call eq?.94 eq?.94 x.19 y.20)
+                            (let ((z.21 (closure-call identity.13 identity.13 1)))
+                              z.21)
+                            (let ((n.22 (closure-call identity.13 identity.13 1)))
+                              (let ((z.23 (closure-call |-.97| |-.97| x.19 n.22)))
+                                (let ((y.24 (closure-call fact.14 fact.14 z.23)))
+                                  (closure-call *.103 *.103 x.19 y.24))))))))))
+          (cletrec
+           ((*.103 (make-closure L.*.103.7 2))
+            (|+.100| (make-closure L.+.100.8 2))
+            (|-.97| (make-closure L.-.97.9 2))
+            (eq?.94 (make-closure L.eq?.94.10 2))
+            (identity.13
+             (make-closure L.identity.13.11 1 eq?.94 |+.100| identity.13 |-.97|))
+            (fact.14
+             (make-closure
+              L.fact.14.12
+              1
+              eq?.94
+              identity.13
+              *.103
+              fact.14
+              |-.97|
+              identity.13
+              identity.13
+              identity.13)))
+           (closure-call fact.14 fact.14 5))))
+   "Check for duplicate let bindings"))
--- passes/uncover-free.rkt
+++ passes/uncover-free.rkt
@@ -19,25 +19,25 @@
   ;; helper compiles the list of values in lam-opticon-lang-v9 to lam-free-lang-v9
   (define (traverse-values-list vs env)
     (define-values (reversed-vs^ all-free-vars)
-      (for/fold ([acc '()]
-                 [free-vars-acc '()])
-                ([v vs])
+      (for/foldr ([acc '()]
+                  [free-vars-acc '()])
+        ([v vs])
         (define-values (v^ val-free-vars) (uncover-free-value v env))
         (values (cons v^ acc)
-                (append val-free-vars free-vars-acc))))
-    (values (reverse reversed-vs^) all-free-vars))
+                (set-union val-free-vars free-vars-acc))))
+    (values reversed-vs^ all-free-vars))
 
   ;; (Listof lam-opticon-lang-v9.effect) -> (Listof lam-free-lang-v9.effect)
   ;; helper compiles the list of values in lam-opticon-lang-v9 to lam-free-lang-v9
   (define (traverse-effects-list effects env)
     (define-values (reversed-es^ all-free-vars)
-      (for/fold ([acc '()]
-                 [free-vars-acc '()])
-                ([e effects])
+      (for/foldr ([acc '()]
+                  [free-vars-acc '()])
+        ([e effects])
         (define-values (e^ effect-free-vars) (uncover-free-effect e env))
         (values (cons e^ acc)
-                (append effect-free-vars free-vars-acc))))
-    (values (reverse reversed-es^) all-free-vars))
+                (set-union effect-free-vars free-vars-acc))))
+    (values reversed-es^ all-free-vars))
 
 
   ;; (Listof alocs) (Listof lam-opticon-lang-v9.value) -> (Listof aloc lam-free-lang-v9.value)
@@ -45,10 +45,10 @@
   ;; TODO: unbound var inside of value in aloc -> vs matching
   (define (traverse-letrec-bindings alocs vs env)
     (define-values (bindings free-variables)
-      (for/fold ([binding-acc '()]
-                 [free-vars-acc '()])
-                ([aloc alocs]
-                 [val vs])
+      (for/foldr ([binding-acc '()]
+                  [free-vars-acc '()])
+        ([aloc alocs]
+         [val vs])
         (match val
           [`(lambda (,args ...) ,body)
            (define env^ (extend-env* env args args))
@@ -56,21 +56,21 @@
            (define info (info-set '() 'free free-vars))
            (values (cons `(,aloc (lambda ,info ,args ,body^)) binding-acc)
                    (append free-vars free-vars-acc))])))
-    (values (reverse bindings) free-variables))
+    (values bindings free-variables))
 
   ;; (Listof alocs) (Listof lam-opticon-lang-v9.value) -> (Listof aloc lam-free-lang-v9.value)
   ;; helper compiles the list of values in lam-opticon-lang-v9 to lam-free-lang-v9
   ;; TODO: unbound var inside of value in aloc -> vs matching
   (define (traverse-bindings-list alocs vs env)
     (define-values (bindings free-variables)
-      (for/fold ([binding-acc '()]
-                 [free-vars-acc '()])
-                ([aloc alocs]
-                 [val vs])
+      (for/foldr ([binding-acc '()]
+                  [free-vars-acc '()])
+        ([aloc alocs]
+         [val vs])
         (define-values (val^ free-vars) (uncover-free-value val env))
         (values (cons `(,aloc ,val^) binding-acc)
-                (append free-vars free-vars-acc))))
-    (values (reverse bindings) free-variables))
+                (set-union free-vars free-vars-acc))))
+    (values bindings free-variables))
 
   ;; lam-opticon-lang-v9.value (Envof alocs)  -> lam-free-lang-v9.value (Listof alocs)
   ;; compiles the values in lam-opticon-lang-v9 to lam-free-lang-v9
@@ -88,8 +88,7 @@
                                        (define-values (body^ free-vars) (uncover-free-value body env^))
                                        (values `(let ,bindings ,body^) (append free-vars free-var-binding))]
       [`(letrec ([,alocs ,vs] ...) ,body) (define-values (bindings free-var-binding) (traverse-letrec-bindings alocs vs env))
-                                          (define env^ (extend-env* env alocs vs))
-                                          (define-values (body^ free-vars) (uncover-free-value body env^))
+                                          (define-values (body^ free-vars) (uncover-free-value body env))
                                           (values `(letrec ,bindings  ,body^)
                                                   (remove* alocs (append free-var-binding free-vars) ))]
       [`(unsafe-procedure-call ,vs ...) (define-values (vs^ free-vars) (traverse-values-list vs env))
@@ -261,4 +260,116 @@
                                   a.3))))
                       a.2))))
           a.1))
-   "Test with super nested letrec with let"))
+   "Test with super nested letrec with let")
+
+  (check-equal?
+   (uncover-free '(module
+                      (letrec ((car.60
+                                (lambda (tmp.61)
+                                  (if (pair? tmp.61) (unsafe-car tmp.61) (error 12))))
+                               (|+.64|
+                                (lambda (tmp.65 tmp.66)
+                                  (if (fixnum? tmp.65)
+                                      (if (fixnum? tmp.66) (unsafe-fx+ tmp.65 tmp.66) (error 2))
+                                      (error 2))))
+                               (eq?.54 (lambda (tmp.55 tmp.56) (eq? tmp.55 tmp.56)))
+                               (cdr.62
+                                (lambda (tmp.63)
+                                  (if (pair? tmp.63) (unsafe-cdr tmp.63) (error 13))))
+                               (cons.57 (lambda (tmp.58 tmp.59) (cons tmp.58 tmp.59)))
+                               (map.25
+                                (lambda (f.26 ls.27)
+                                  (if (unsafe-procedure-call eq?.54 empty ls.27)
+                                      empty
+                                      (unsafe-procedure-call
+                                       cons.57
+                                       (if (procedure? f.26)
+                                           (if (eq? (unsafe-procedure-arity f.26) 1)
+                                               (unsafe-procedure-call
+                                                f.26
+                                                (unsafe-procedure-call car.60 ls.27))
+                                               (error 42))
+                                           (error 43))
+                                       (unsafe-procedure-call
+                                        map.25
+                                        f.26
+                                        (unsafe-procedure-call cdr.62 ls.27)))))))
+                        (unsafe-procedure-call
+                         map.25
+                         (letrec ((lam.7 (lambda (x.28) (unsafe-procedure-call |+.64| 1 x.28))))
+                           lam.7)
+                         (unsafe-procedure-call
+                          cons.57
+                          1
+                          (unsafe-procedure-call
+                           cons.57
+                           2
+                           (unsafe-procedure-call cons.57 3 empty)))))))
+   '(module
+        (letrec ((car.60
+                  (lambda ((free ()))
+                    (tmp.61)
+                    (if (pair? tmp.61) (unsafe-car tmp.61) (error 12))))
+                 (|+.64|
+                  (lambda ((free ()))
+                    (tmp.65 tmp.66)
+                    (if (fixnum? tmp.65)
+                        (if (fixnum? tmp.66) (unsafe-fx+ tmp.65 tmp.66) (error 2))
+                        (error 2))))
+                 (eq?.54 (lambda ((free ())) (tmp.55 tmp.56) (eq? tmp.55 tmp.56)))
+                 (cdr.62
+                  (lambda ((free ()))
+                    (tmp.63)
+                    (if (pair? tmp.63) (unsafe-cdr tmp.63) (error 13))))
+                 (cons.57 (lambda ((free ())) (tmp.58 tmp.59) (cons tmp.58 tmp.59)))
+                 (map.25
+                  (lambda ((free (eq?.54 car.60 cdr.62 map.25 cons.57)))
+                    (f.26 ls.27)
+                    (if (unsafe-procedure-call eq?.54 empty ls.27)
+                        empty
+                        (unsafe-procedure-call
+                         cons.57
+                         (if (procedure? f.26)
+                             (if (eq? (unsafe-procedure-arity f.26) 1)
+                                 (unsafe-procedure-call
+                                  f.26
+                                  (unsafe-procedure-call car.60 ls.27))
+                                 (error 42))
+                             (error 43))
+                         (unsafe-procedure-call
+                          map.25
+                          f.26
+                          (unsafe-procedure-call cdr.62 ls.27)))))))
+          (unsafe-procedure-call
+           map.25
+           (letrec ((lam.7
+                     (lambda ((free (|+.64|)))
+                       (x.28)
+                       (unsafe-procedure-call |+.64| 1 x.28))))
+             lam.7)
+           (unsafe-procedure-call
+            cons.57
+            1
+            (unsafe-procedure-call
+             cons.57
+             2
+             (unsafe-procedure-call cons.57 3 empty))))))
+   "More complicated test from dox-lambdas")
+  (check-equal?
+   (uncover-free
+    `(module
+         (letrec ([x.1 (lambda () (let ([a.2 (unsafe-procedure-call identify.13 identify.13)]
+                                        [a.3 (unsafe-procedure-call identify.13 identify.13)]
+                                        [a.4 (unsafe-procedure-call identify.13 identify.13)])
+                                    a.4))])
+           x.1)))
+   '(module
+        (letrec ((x.1
+                  (lambda ((free (identify.13)))
+                    ()
+                    (let ((a.2 (unsafe-procedure-call identify.13 identify.13))
+                          (a.3 (unsafe-procedure-call identify.13 identify.13))
+                          (a.4 (unsafe-procedure-call identify.13 identify.13)))
+                      a.4))))
+          x.1))
+   "Test, let with duplicate bindings"))
