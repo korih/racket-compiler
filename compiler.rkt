4dd6dc3c4432817831b83ba708830204ad41b4d3
Lance Tan <ltan0521@gmail.com>
2025-04-08 16:46:45 -0700

    Update provide statement

--- compiler.rkt
+++ compiler.rkt
@@ -45,7 +45,42 @@
   "passes/implement-closures.rkt"
   "passes/expand-macros.rkt")
 
-(provide (all-defined-out))
+(provide
+ expand-macros
+ uniquify
+ implement-safe-primops
+ implement-safe-call
+ define->letrec
+ optimize-direct-calls
+ dox-lambdas
+ uncover-free
+ convert-closures
+ optimize-known-calls
+ hoist-lambdas
+ implement-closures
+ specify-representation
+ remove-complex-opera*
+ sequentialize-let
+ normalize-bind
+ impose-calling-conventions
+ select-instructions
+ expose-allocation-pointer
+ uncover-locals
+ undead-analysis
+ conflict-analysis
+ assign-call-undead-variables
+ allocate-frames
+ assign-registers
+ assign-frame-variables
+ replace-locations
+ implement-fvars
+ optimize-predicates
+ expose-basic-blocks
+ resolve-predicates
+ flatten-program
+ patch-instructions
+ implement-mops
+ generate-x64)
 
 ;; You can modify this pass list, e.g., by adding other
 ;; optimization, debugging, or validation passes.
