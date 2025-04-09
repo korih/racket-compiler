db2928c8f84a64956300d285add54ab7f630b4bb
itsarune <42703774+itsarune@users.noreply.github.com>
2025-04-09 16:17:51 -0700

    Fix some more duplicated identifiers in uncover-free

--- tests/uncover-free.rkt
+++ tests/uncover-free.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require
+  cpsc411/langs/v9
   rackunit
   "../passes/uncover-free.rkt")
 
@@ -138,10 +139,10 @@
                         a.1)))
    '(module
         (letrec ((a.1
-                  (lambda ((free (tmp.3 tmp.4)))
+                  (lambda ((free (tmp.4 tmp.3)))
                     ()
                     (letrec ((a.2
-                              (lambda ((free (tmp.3 tmp.4)))
+                              (lambda ((free (tmp.4 tmp.3)))
                                 ()
                                 (let ((a.3
                                        (begin (eq? 1 2) (if (eq? 2 1) tmp.3 tmp.4))))
@@ -151,97 +152,97 @@
    "Test with super nested letrec with let")
 
   (check-equal?
-   (uncover-free '(module
-                      (letrec ((car.60
-                                (lambda (tmp.61)
-                                  (if (pair? tmp.61) (unsafe-car tmp.61) (error 12))))
-                               (|+.64|
-                                (lambda (tmp.65 tmp.66)
-                                  (if (fixnum? tmp.65)
-                                      (if (fixnum? tmp.66) (unsafe-fx+ tmp.65 tmp.66) (error 2))
-                                      (error 2))))
-                               (eq?.54 (lambda (tmp.55 tmp.56) (eq? tmp.55 tmp.56)))
-                               (cdr.62
-                                (lambda (tmp.63)
-                                  (if (pair? tmp.63) (unsafe-cdr tmp.63) (error 13))))
-                               (cons.57 (lambda (tmp.58 tmp.59) (cons tmp.58 tmp.59)))
-                               (map.25
-                                (lambda (f.26 ls.27)
-                                  (if (unsafe-procedure-call eq?.54 empty ls.27)
-                                      empty
-                                      (unsafe-procedure-call
-                                       cons.57
-                                       (if (procedure? f.26)
-                                           (if (eq? (unsafe-procedure-arity f.26) 1)
-                                               (unsafe-procedure-call
-                                                f.26
-                                                (unsafe-procedure-call car.60 ls.27))
-                                               (error 42))
-                                           (error 43))
-                                       (unsafe-procedure-call
-                                        map.25
-                                        f.26
-                                        (unsafe-procedure-call cdr.62 ls.27)))))))
-                        (unsafe-procedure-call
-                         map.25
-                         (letrec ((lam.7 (lambda (x.28) (unsafe-procedure-call |+.64| 1 x.28))))
-                           lam.7)
-                         (unsafe-procedure-call
-                          cons.57
-                          1
-                          (unsafe-procedure-call
-                           cons.57
-                           2
-                           (unsafe-procedure-call cons.57 3 empty)))))))
-   '(module
-        (letrec ((car.60
-                  (lambda ((free ()))
-                    (tmp.61)
-                    (if (pair? tmp.61) (unsafe-car tmp.61) (error 12))))
-                 (|+.64|
-                  (lambda ((free ()))
-                    (tmp.65 tmp.66)
-                    (if (fixnum? tmp.65)
-                        (if (fixnum? tmp.66) (unsafe-fx+ tmp.65 tmp.66) (error 2))
-                        (error 2))))
-                 (eq?.54 (lambda ((free ())) (tmp.55 tmp.56) (eq? tmp.55 tmp.56)))
-                 (cdr.62
-                  (lambda ((free ()))
-                    (tmp.63)
-                    (if (pair? tmp.63) (unsafe-cdr tmp.63) (error 13))))
-                 (cons.57 (lambda ((free ())) (tmp.58 tmp.59) (cons tmp.58 tmp.59)))
-                 (map.25
-                  (lambda ((free (eq?.54 car.60 cdr.62 map.25 cons.57)))
-                    (f.26 ls.27)
-                    (if (unsafe-procedure-call eq?.54 empty ls.27)
-                        empty
-                        (unsafe-procedure-call
-                         cons.57
-                         (if (procedure? f.26)
-                             (if (eq? (unsafe-procedure-arity f.26) 1)
-                                 (unsafe-procedure-call
-                                  f.26
-                                  (unsafe-procedure-call car.60 ls.27))
-                                 (error 42))
-                             (error 43))
-                         (unsafe-procedure-call
-                          map.25
-                          f.26
-                          (unsafe-procedure-call cdr.62 ls.27)))))))
-          (unsafe-procedure-call
-           map.25
-           (letrec ((lam.7
-                     (lambda ((free (|+.64|)))
-                       (x.28)
-                       (unsafe-procedure-call |+.64| 1 x.28))))
-             lam.7)
-           (unsafe-procedure-call
-            cons.57
-            1
-            (unsafe-procedure-call
-             cons.57
-             2
-             (unsafe-procedure-call cons.57 3 empty))))))
+   (interp-lam-free-lang-v9 (uncover-free '(module
+                                               (letrec ((car.60
+                                                         (lambda (tmp.61)
+                                                           (if (pair? tmp.61) (unsafe-car tmp.61) (error 12))))
+                                                        (|+.64|
+                                                         (lambda (tmp.65 tmp.66)
+                                                           (if (fixnum? tmp.65)
+                                                               (if (fixnum? tmp.66) (unsafe-fx+ tmp.65 tmp.66) (error 2))
+                                                               (error 2))))
+                                                        (eq?.54 (lambda (tmp.55 tmp.56) (eq? tmp.55 tmp.56)))
+                                                        (cdr.62
+                                                         (lambda (tmp.63)
+                                                           (if (pair? tmp.63) (unsafe-cdr tmp.63) (error 13))))
+                                                        (cons.57 (lambda (tmp.58 tmp.59) (cons tmp.58 tmp.59)))
+                                                        (map.25
+                                                         (lambda (f.26 ls.27)
+                                                           (if (unsafe-procedure-call eq?.54 empty ls.27)
+                                                               empty
+                                                               (unsafe-procedure-call
+                                                                cons.57
+                                                                (if (procedure? f.26)
+                                                                    (if (eq? (unsafe-procedure-arity f.26) 1)
+                                                                        (unsafe-procedure-call
+                                                                         f.26
+                                                                         (unsafe-procedure-call car.60 ls.27))
+                                                                        (error 42))
+                                                                    (error 43))
+                                                                (unsafe-procedure-call
+                                                                 map.25
+                                                                 f.26
+                                                                 (unsafe-procedure-call cdr.62 ls.27)))))))
+                                                 (unsafe-procedure-call
+                                                  map.25
+                                                  (letrec ((lam.7 (lambda (x.28) (unsafe-procedure-call |+.64| 1 x.28))))
+                                                    lam.7)
+                                                  (unsafe-procedure-call
+                                                   cons.57
+                                                   1
+                                                   (unsafe-procedure-call
+                                                    cons.57
+                                                    2
+                                                    (unsafe-procedure-call cons.57 3 empty))))))))
+   (interp-lam-free-lang-v9 '(module
+                                 (letrec ((car.60
+                                           (lambda ((free ()))
+                                             (tmp.61)
+                                             (if (pair? tmp.61) (unsafe-car tmp.61) (error 12))))
+                                          (|+.64|
+                                           (lambda ((free ()))
+                                             (tmp.65 tmp.66)
+                                             (if (fixnum? tmp.65)
+                                                 (if (fixnum? tmp.66) (unsafe-fx+ tmp.65 tmp.66) (error 2))
+                                                 (error 2))))
+                                          (eq?.54 (lambda ((free ())) (tmp.55 tmp.56) (eq? tmp.55 tmp.56)))
+                                          (cdr.62
+                                           (lambda ((free ()))
+                                             (tmp.63)
+                                             (if (pair? tmp.63) (unsafe-cdr tmp.63) (error 13))))
+                                          (cons.57 (lambda ((free ())) (tmp.58 tmp.59) (cons tmp.58 tmp.59)))
+                                          (map.25
+                                           (lambda ((free (eq?.54 car.60 cdr.62 map.25 cons.57)))
+                                             (f.26 ls.27)
+                                             (if (unsafe-procedure-call eq?.54 empty ls.27)
+                                                 empty
+                                                 (unsafe-procedure-call
+                                                  cons.57
+                                                  (if (procedure? f.26)
+                                                      (if (eq? (unsafe-procedure-arity f.26) 1)
+                                                          (unsafe-procedure-call
+                                                           f.26
+                                                           (unsafe-procedure-call car.60 ls.27))
+                                                          (error 42))
+                                                      (error 43))
+                                                  (unsafe-procedure-call
+                                                   map.25
+                                                   f.26
+                                                   (unsafe-procedure-call cdr.62 ls.27)))))))
+                                   (unsafe-procedure-call
+                                    map.25
+                                    (letrec ((lam.7
+                                              (lambda ((free (|+.64|)))
+                                                (x.28)
+                                                (unsafe-procedure-call |+.64| 1 x.28))))
+                                      lam.7)
+                                    (unsafe-procedure-call
+                                     cons.57
+                                     1
+                                     (unsafe-procedure-call
+                                      cons.57
+                                      2
+                                      (unsafe-procedure-call cons.57 3 empty)))))))
    "More complicated test from dox-lambdas")
   (check-equal?
    (uncover-free
