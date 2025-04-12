7eff3dc83688d5739a1d2d4eb964056b1c40c45a
Lance Tan <ltan0521@gmail.com>
2025-04-11 17:48:47 -0700

    Fix integration tests

--- integration-test.rkt
+++ integration-test.rkt
@@ -1,13 +1,9 @@
 #lang racket
 
 (require
-  "passes/assign-fvars.rkt"
-  "passes/assign-homes-opt.rkt"
-  "passes/assign-homes.rkt"
   "passes/assign-call-undead-variables.rkt"
   "passes/assign-registers.rkt"
   "passes/conflict-analysis.rkt"
-  "passes/flatten-begins.rkt"
   "passes/implement-fvars.rkt"
   "passes/normalize-bind.rkt"
   "passes/patch-instructions.rkt"
@@ -22,8 +18,6 @@
   "passes/optimize-predicates.rkt"
   "passes/flatten-program.rkt"
   "passes/expose-basic-blocks.rkt"
-  "passes/link-paren-x64.rkt"
-  "passes/interp-paren-x64.rkt"
   "passes/impose-calling-conventions.rkt"
   "passes/assign-call-undead-variables.rkt"
   "passes/allocate-frames.rkt"
@@ -32,111 +26,142 @@
   "passes/specify-representation.rkt"
   "passes/remove-complex-opera.rkt"
   "passes/expose-allocation-pointer.rkt"
-  "passes/implement-mops.rkt")
+  "passes/implement-mops.rkt"
+  "passes/implement-safe-call.rkt"
+  "passes/define-letrec.rkt"
+  "passes/optimize-direct-calls.rkt"
+  "passes/dox-lambdas.rkt"
+  "passes/uncover-free.rkt"
+  "passes/convert-closures.rkt"
+  "passes/optimize-known-calls.rkt"
+  "passes/hoist-lambdas.rkt"
+  "passes/implement-closures.rkt"
+  "passes/expand-macros.rkt")
 
 (module+ test
   (require cpsc411/compiler-lib
            cpsc411/langs/v8
+           cpsc411/langs/v9
+           cpsc411/langs/v11
            cpsc411/ptr-run-time
-           cpsc411/test-suite/public/v8
-           rackunit)
+           cpsc411/test-suite/utils
+           rackunit
+           rackunit/text-ui)
 
-  (define tests (set
-                 '(module
-                      (define add (lambda (x y) (call + x y)))
-                    (call add 10 20))
-                 '(module
-                      (define abs (lambda (x) (if (call < x 0) (call - 0 x) x)))
-                    (call abs -5))
-                 '(module
-                      (define check-empty (lambda (x) (if (call empty? x) #t (error 1))))
-                    (call check-empty empty))
-                 '(module
-                      (define v (lambda () (call make-vector 3)))
-                    (define set-first (lambda (vec) (call vector-set! vec 0 42)))
-                    (define get-first (lambda (vec) (call vector-ref vec 0)))
+  (register-test-programs!
+   interp-racketish-surface
+   `(("quicksort"
+      (module
+          (define filter
+            (lambda (f ls)
+              (if (empty? ls)
+                  ls
+                  (let ([x (car ls)])
+                    ((lambda (y)
+                       (if (f x)
+                           (cons x y)
+                           y))
+                     (filter f (cdr ls)))))))
 
-                    (let ([vec (call v)])
-                      (call + (if (call void? (call set-first vec)) 0 (error 1))
-                            (call get-first vec))))
-                 '(module
-                      (define factorial (lambda (n)
-                                          (if (call <= n 1)
-                                              1
-                                              (call * n (call factorial (call - n 1))))))
-                    (call factorial 5))
-                 ))
+        (define append
+          (lambda (ls1 ls2)
+            (if (empty? ls1)
+                ls2
+                (cons (car ls1) (append (cdr ls1) ls2)))))
+
+        (define quicksort
+          (lambda (ls)
+            (if (or (empty? ls)
+                    (empty? (cdr ls)))
+                ls
+                (let ([pivot (car ls)]
+                      [rst (cdr ls)])
+                  (append
+                   (append
+                    (quicksort
+                     (filter (lambda (x) (< x pivot)) rst))
+                    (cons pivot empty))
+                   (quicksort
+                    (filter (lambda (x) (>= x pivot)) rst)))))))
+
+        (define mod
+          (lambda (x y)
+            (if (>= x y)
+                (mod (- x y) y)
+                x)))
+
+        (define build-list
+          (lambda (f len)
+            (if (eq? len 0)
+                empty
+                (cons (f len) (build-list f (- len 1))))))
+
+        (define sorted?
+          (lambda (ls)
+            (if (eq? empty ls)
+                #t
+                (if (eq? empty (cdr ls))
+                    #t
+                    (and (< (car ls) (car (cdr ls)))
+                         (sorted? (cdr (cdr ls))))))))
+
+        (let ([x (make-vector 1)])
+          (let ([random (lambda ()
+                          (let ([A 12312]
+                                [C 1]
+                                [MAX 100])
+                            (let ([p (mod
+                                      (+ C (* A (vector-ref x 0)))
+                                      MAX)])
+                              (begin
+                                (vector-set! x 0 p)
+                                p))))])
+            (sorted? (quicksort (build-list (lambda (x) (random)) 20)))))))))
 
   (define pass-map
     (list
-     (cons uniquify interp-exprs-unique-lang-v8)
-     (cons implement-safe-primops interp-exprs-unsafe-data-lang-v8)
-     (cons specify-representation interp-exprs-bits-lang-v8)
-     (cons remove-complex-opera* interp-values-bits-lang-v8)
-     (cons sequentialize-let interp-imp-mf-lang-v8)
-     (cons normalize-bind interp-proc-imp-cmf-lang-v8)
-     (cons impose-calling-conventions interp-imp-mf-lang-v8)
-     (cons select-instructions interp-asm-alloc-lang-v8)
-     (cons expose-allocation-pointer interp-asm-pred-lang-v8)
-     (cons uncover-locals interp-asm-pred-lang-v8/locals)
-     (cons undead-analysis interp-asm-pred-lang-v8/undead)
-     (cons conflict-analysis interp-asm-pred-lang-v8/conflicts)
-     (cons assign-call-undead-variables interp-asm-pred-lang-v8/pre-framed)
-     (cons allocate-frames interp-asm-pred-lang-v8/framed)
-     (cons assign-registers interp-asm-pred-lang-v8/spilled)
-     (cons assign-frame-variables interp-asm-pred-lang-v8/assignments)
-     (cons replace-locations interp-nested-asm-lang-fvars-v8)
+     (cons expand-macros interp-racketish-surface)
+     (cons uniquify interp-exprs-lang-v9)
+     (cons implement-safe-primops interp-exprs-unique-lang-v9)
+     (cons implement-safe-call interp-exprs-unsafe-data-lang-v9)
+     (cons define->letrec interp-exprs-unsafe-lang-v9)
+     (cons optimize-direct-calls interp-just-exprs-lang-v9)
+     (cons dox-lambdas interp-just-exprs-lang-v9)
+     (cons uncover-free interp-lam-opticon-lang-v9)
+     (cons convert-closures interp-lam-free-lang-v9)
+     (cons optimize-known-calls interp-closure-lang-v9)
+     (cons hoist-lambdas interp-closure-lang-v9)
+     (cons implement-closures interp-hoisted-lang-v9)
+     (cons specify-representation interp-proc-exposed-lang-v9)
+     (cons remove-complex-opera* interp-exprs-bits-lang-v8)
+     (cons sequentialize-let interp-values-bits-lang-v8)
+     (cons normalize-bind interp-imp-mf-lang-v8)
+     (cons impose-calling-conventions interp-proc-imp-cmf-lang-v8)
+     (cons select-instructions interp-imp-cmf-lang-v8)
+     (cons expose-allocation-pointer interp-asm-alloc-lang-v8)
+     (cons uncover-locals interp-asm-pred-lang-v8)
+     (cons undead-analysis interp-asm-pred-lang-v8/locals)
+     (cons conflict-analysis interp-asm-pred-lang-v8/undead)
+     (cons assign-call-undead-variables interp-asm-pred-lang-v8/conflicts)
+     (cons allocate-frames interp-asm-pred-lang-v8/pre-framed)
+     (cons assign-registers interp-asm-pred-lang-v8/framed)
+     (cons assign-frame-variables interp-asm-pred-lang-v8/spilled)
+     (cons replace-locations interp-asm-pred-lang-v8/assignments)
      (cons optimize-predicates interp-nested-asm-lang-fvars-v8)
-     (cons implement-fvars interp-nested-asm-lang-v8)
-     (cons expose-basic-blocks interp-block-pred-lang-v8)
-     (cons resolve-predicates interp-block-asm-lang-v8)
-     (cons flatten-program interp-para-asm-lang-v8)
-     (cons patch-instructions interp-paren-x64-mops-v8)
-     (cons implement-mops interp-paren-x64-v8)
-     (cons generate-x64 #f)
+     (cons implement-fvars interp-nested-asm-lang-fvars-v8)
+     (cons expose-basic-blocks interp-nested-asm-lang-v8)
+     (cons resolve-predicates interp-block-pred-lang-v8)
+     (cons flatten-program interp-block-asm-lang-v8)
+     (cons patch-instructions interp-para-asm-lang-v8)
+     (cons implement-mops interp-paren-x64-mops-v8)
+     (cons generate-x64 interp-paren-x64-v8)
      (cons wrap-x64-boilerplate #f)
      (cons wrap-x64-run-time #f)))
 
-  (for ([test tests])
-    (for/fold ([pass-list empty]
-               [last-program test])
-              ([pass-pair pass-map])
-      (define pass-list^ (append pass-list (list (car pass-pair))))
-      (define interpreter (cdr pass-pair))
-      (define compiled test)
-      (parameterize ([current-pass-list pass-list^])
-        (set! compiled (compile test))
-        (when interpreter
-          (check-equal? (interpreter compiled) (interp-exprs-lang-v8 test) (format "error in ~a \n input: ~a \n output: ~a" (car pass-pair) last-program compiled))))
-      (values pass-list^ compiled)))
+  (define pass-list (map car pass-map))
+  (define interp-list (map cdr pass-map))
 
-  (parameterize ([current-pass-list (list
-                                     uniquify
-                                     implement-safe-primops
-                                     specify-representation
-                                     remove-complex-opera*
-                                     sequentialize-let
-                                     normalize-bind
-                                     impose-calling-conventions
-                                     select-instructions
-                                     expose-allocation-pointer
-                                     uncover-locals
-                                     undead-analysis
-                                     conflict-analysis
-                                     assign-call-undead-variables
-                                     allocate-frames
-                                     assign-registers
-                                     assign-frame-variables
-                                     replace-locations
-                                     optimize-predicates
-                                     implement-fvars
-                                     expose-basic-blocks
-                                     resolve-predicates
-                                     flatten-program
-                                     patch-instructions
-                                     implement-mops
-                                     generate-x64
-                                     wrap-x64-boilerplate
-                                     wrap-x64-run-time)])
-    (for ([test tests])
-      (check-equal? (execute test) (interp-exprs-lang-v8 test) (format "input: ~a" test)))))
+  (run-tests
+   (test-suite
+    "integration test suite"
+    (compiler-testomatic pass-list interp-list))))
